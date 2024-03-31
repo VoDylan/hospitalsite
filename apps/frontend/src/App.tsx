c15fa0e1297c75caeb7343bf6c12a7ef806f2c34
EleneKaji <kajaiaelene@gmail.com>
2024-03-31 00:16:05 -0400

    Implementing map page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Map from "./routes/map.tsx";
-// import TestAnimation from "./routes/testAnimation.tsx";
-// import MapWithMouseCoordinates from "./routes/mapWithMouseCoordinates.tsx";
 
 function App() {
   const router = createBrowserRouter([
