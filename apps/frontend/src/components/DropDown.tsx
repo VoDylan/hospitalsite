543955b90a18c1ff26e6e7563262d7119ee2d3b2
jjmurphy1114 <jjmurphy1114@outlook.com>
2024-03-30 21:12:25 -0400

    Added text boxes and drop down. They are not displaying yet but they are in the FlowerDeliveryService file

--- apps/frontend/src/components/DropDown.tsx
+++ apps/frontend/src/components/DropDown.tsx
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
 import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 
 interface DropDownProps {
   items: string[];
+  handleChange: (event: SelectChangeEvent) => void;
 }
 
 export function DropDown(props: DropDownProps) {
-  const [value, setValue] = useState("");
-  const handleChange = (event: SelectChangeEvent) => {
-    setValue(event.target.value as string);
-  };
-
   return (
     <Select
-      onChange={handleChange}
-      value={value}
+      onChange={props.handleChange}
       inputProps={{ "aria-label": "Without label" }}
     >
       {props.items.map((item) => (
--- apps/frontend/src/routes/FlowerDeliveryService.tsx
+++ apps/frontend/src/routes/FlowerDeliveryService.tsx
@@ -1,10 +1,48 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, SelectChangeEvent, Typography } from "@mui/material";
+import { DropDown } from "../components/DropDown.tsx";
+import Textbox from "../components/Textbox.tsx";
+import { FlowerDeliveryFormSubmission } from "../common/FlowerDeliveryFormSubmission.ts";
+import { ChangeEvent, useState } from "react";
 function FlowerDeliveryService() {
+  const [form, setResponses] = useState<FlowerDeliveryFormSubmission>({
+    name: "",
+    flowerType: "",
+    recipientName: "",
+    roomNumber: "",
+    message: "",
+  });
+
+  function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
+    setResponses({ ...form, name: e.target.value });
+  }
+
+  function handleRecipientNameInput(e: ChangeEvent<HTMLInputElement>) {
+    setResponses({ ...form, recipientName: e.target.value });
+  }
+
+  function handleRoomNumberInput(e: ChangeEvent<HTMLInputElement>) {
+    setResponses({ ...form, roomNumber: e.target.value });
+  }
+
+  function handleMessageInput(e: ChangeEvent<HTMLInputElement>) {
+    setResponses({ ...form, message: e.target.value });
+  }
+
+  // function clear(){
+  //     setResponses({name: "", flowerType: "", recipientName: "", roomNumber: "", message: ""});
+  // }
+
+  // For dropdown
+  const handleFlowerTypeInput = (event: SelectChangeEvent) => {
+    setResponses({ ...form, flowerType: event.target.value as string });
+  };
+
   return (
     <>
       <Grid
         container
         my={8}
+        mx={6}
         rowSpacing={8}
         columnSpacing={4}
         alignContent={"center"}
@@ -31,27 +69,47 @@ function FlowerDeliveryService() {
         </Grid>
         <Grid item xs={6}>
           <Box>
-            <Typography>Box 2</Typography>
+            <Textbox
+              label={"Name:"}
+              value={form.name}
+              onChange={handleNameInput}
+            />
           </Box>
         </Grid>
         <Grid item xs={6}>
           <Box>
-            <Typography>Box 2</Typography>
+            <Typography>What type of flowers will you be ordering?</Typography>
+            <DropDown
+              items={["Red Carnations", "Red Roses", "White Roses", "Tulips"]}
+              handleChange={handleFlowerTypeInput}
+            />
           </Box>
         </Grid>
         <Grid item xs={6}>
           <Box>
-            <Typography>Box 3</Typography>
+            <Textbox
+              label={"Recipient Name:"}
+              value={form.recipientName}
+              onChange={handleRecipientNameInput}
+            />
           </Box>
         </Grid>
         <Grid item xs={6}>
           <Box>
-            <Typography>Box 4</Typography>
+            <Textbox
+              label={"Room Number:"}
+              value={form.roomNumber}
+              onChange={handleRoomNumberInput}
+            />
           </Box>
         </Grid>
         <Grid item xs={6}>
           <Box>
-            <Typography>Box 5</Typography>
+            <Textbox
+              label={"Add a message (optional)"}
+              value={form.message}
+              onChange={handleMessageInput}
+            />
           </Box>
         </Grid>
         <Grid item xs={12}>
