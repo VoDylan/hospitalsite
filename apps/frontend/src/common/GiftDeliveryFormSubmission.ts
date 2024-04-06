7204b4eba2907b54067693c55adf93c23d70f337
erichong1113 <20021113hyt@gmail.com>
2024-04-06 18:59:29 -0400

    GiftDelivery framework

---
+++ apps/frontend/src/common/GiftDeliveryFormSubmission.ts
@@ -0,0 +1,8 @@
+export type GiftDeliveryFormSubmission = {
+  name: string;
+  recipientName: string;
+  roomNumber: string;
+  giftSize: string;
+  message: string;
+  extraFee: string;
+};
---
+++ apps/frontend/src/routes/GiftDeliveryService.tsx
@@ -0,0 +1,78 @@
+import { Grid, Stack } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import { LeftAlignedTextbox } from "../components/LeftAlignedTextbox.tsx";
+import RadioButtonsGroup from "../components/RadioButtonsGroup.tsx";
+import { DropDown } from "../components/DropDown.tsx";
+import { GiftDeliveryFormSubmission } from "../common/GiftDeliveryFormSubmission.ts";
+import TopBanner from "../components/TopBanner.tsx";
+
+function GiftDeliveryService() {
+  const [form, setFormResponses] = useState<GiftDeliveryFormSubmission>({
+    name: "",
+    recipientName: "",
+    roomNumber: "",
+    giftSize: "",
+    message: "",
+    extraFee: "",
+  });
+
+  function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, name: e.target.value });
+  }
+
+  function handleRecipientNameInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, recipientName: e.target.value });
+  }
+
+  function handleroomNumberInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, roomNumber: e.target.value });
+  }
+
+  function handlegiftSizeInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, giftSize: e.target.value });
+  }
+  function handlemessageInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, message: e.target.value });
+  }
+  function handleextreFeeInput(e: ChangeEvent<HTMLInputElement>) {
+    setFormResponses({ ...form, extraFee: e.target.value });
+  }
+
+  return (
+    <Stack
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      spacing={20}
+      sx={{
+        width: "100vw",
+        height: "auto",
+        display: "flex",
+        alignItems: "center", // Center vertically
+        justifyContent: "center", // Center horizontally
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        minHeight: "100vh",
+        maxWidth: "100%",
+        overflowX: "hidden",
+      }}
+    >
+      <TopBanner />
+      <Grid
+        container
+        direction={"row"}
+        rowSpacing={1}
+        columnSpacing={5}
+        justifyContent={"center"}
+        boxShadow={4}
+        sx={{
+          backgroundColor: "white",
+          width: "40vw", //Adjust this to change the width of the form
+          height: "auto",
+        }}
+      ></Grid>
+    </Stack>
+  );
+}
+
+export default GiftDeliveryService;
