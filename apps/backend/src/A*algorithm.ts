1bec9d03e00a8bb87cfba63cd25fb9e167928fcb
EleneKaji <kajaiaelene@gmail.com>
2024-04-07 00:52:09 -0400

    Starting A* algo

--- apps/backend/src/A*algorithm.ts
+++ apps/backend/src/A*algorithm.ts
@@ -1,9 +1,19 @@
 import { NodeAStar } from "common/src/NodeAStar.ts";
+import GraphManager from "common/src/GraphManager.ts";
 
 export class AAlgorithm {
   nodes: NodeAStar[];
 
   public constructor() {
     this.nodes = [];
+  }
+
+  public loadData() {
+    const graphManager = GraphManager.getInstance();
+    const mapNodes = graphManager.nodes;
+    const mapEdges = graphManager.edges;
+
+    console.log(mapEdges);
+    console.log(mapNodes);
   }
 }
