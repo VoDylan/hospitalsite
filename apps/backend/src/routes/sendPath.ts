6f95f84b82a4aa968a99689104dbb3a53a79d779
EleneKaji <kajaiaelene@gmail.com>
2024-04-09 22:06:39 -0400

    Changed BFS algo to return nodeIDs and coordinates. Everything for BFS works accordingly.

--- apps/backend/src/routes/sendPath.ts
+++ apps/backend/src/routes/sendPath.ts
@@ -3,40 +3,23 @@ import express, { Router } from "express";
 // import {Coordinates} from "common/src/Coordinates.ts";
 import { LocationInfo } from "common/src/LocationInfo.ts";
 
-import { AStarAlgorithm } from "../AStarAlgorithm.ts";
+// import { AStarAlgorithm } from "../AStarAlgorithm.ts";
+import { BFSalgorithm } from "../BFSalgorithm.ts";
 
 const router: Router = express.Router();
-
-// router.post("/", async (req, res) => {
-//   const request: LocationInfo = req.body;
-//   const startID = request.startNode;
-//   const endID = request.endNode;
-//
-//   const bfs = new BFSalgorithm();
-//   async function runBfs() {
-//     await bfs.load_data();
-//     return bfs.BFS(startID, endID);
-//   }
-//
-//   const path = await runBfs();
-//
-//   res.status(200).json({
-//     message: path,
-//   });
-// });
 
 router.post("/", async (req, res) => {
   const request: LocationInfo = req.body;
   const startID = request.startNode;
   const endID = request.endNode;
 
-  const algo = new AStarAlgorithm();
-  async function runAStar() {
-    await algo.loadData();
-    return algo.AStar(startID, endID);
+  const bfs = new BFSalgorithm();
+  async function runBfs() {
+    await bfs.load_data();
+    return bfs.BFS(startID, endID);
   }
 
-  const path = await runAStar();
+  const path = await runBfs();
 
   res.status(200).json({
     message: path,
