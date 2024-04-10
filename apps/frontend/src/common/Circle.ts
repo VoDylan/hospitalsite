3da04d08165c1abf3545f5f7988acfa5c8444db9
mwalsh001 <matty71604@gmail.com>
2024-04-10 00:57:53 -0400

    Display all nodes and filter out nodes based on filter selection

---
+++ apps/frontend/src/common/Circle.ts
@@ -0,0 +1,13 @@
+// circle.ts
+export function drawCircle(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number, fillColor: string, strokeColor: string, lineWidth: number) {
+  ctx.fillStyle = fillColor;
+  ctx.strokeStyle = strokeColor;
+  ctx.lineWidth = lineWidth;
+
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.closePath();
+
+  ctx.stroke();
+  ctx.fill();
+}
