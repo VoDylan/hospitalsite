ddd152d134445244a4b693fcbaefc1ad50f7f0ea
VoDylan <vdylan24@gmail.com>
2024-04-18 12:18:22 -0400

    Merged files from main

---
+++ apps/frontend/src/components/map/ClickableCanvas.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from "react";
+import initializeLayeredCanvas from "./InitializeLayeredCanvas.ts";
+
+interface ClickableCanvasProps {
+  style: React.CSSProperties;
+  backgroundRendered: boolean;
+  width: number;
+  height: number;
+  onClick: (event: React.MouseEvent) => void;
+}
+
+export default function ClickableCanvas(props: ClickableCanvasProps) {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (props.backgroundRendered)
+      initializeLayeredCanvas(canvasRef.current, props.width, props.height);
+  }, [props.backgroundRendered, props.height, props.width]);
+
+  return <canvas ref={canvasRef} style={props.style} onClick={props.onClick} />;
+}
