557155ba174762710a7da8b0f5a3a9517822515e
Robert Mellen <ramellen@wpi.edu>
2024-04-18 15:15:34 -0400

    Making entire service card clickable

--- apps/frontend/src/routes/ServicesPage.tsx
+++ apps/frontend/src/routes/ServicesPage.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography, Zoom, Grid } from "@mui/material";
 import { ServiceCard } from "../components/homepage/ServiceCard.tsx";
-import TopBanner2 from "../components/banner/TopBanner2.tsx";
 //import Flower from "../images/servicePageImages/FlowerDelivery.svg";
 //import Medicine from "../images/servicePageImages/MedicineDelivery.svg";
 import sanitation from "../images/servicePageImages/janitorService.jpg";
@@ -20,105 +19,144 @@ export default function ServicesPage() {
 
   // Define service paths and images
   const serviceData = [
-    { path: "/Services/FlowerDelivery",
+    {
+      path: "/Services/FlowerDelivery",
       image: flower,
-      title: 'Flower Delivery',
-      description: 'Deliver a bouquet of flowers to a friend or loved one',
-      buttonContent: 'Get Flowers'},
-    { path: "/Services/MedicineDelivery",
-      image: medicine,
-      title: 'Medicine Delivery',
-      description: 'Get medicine for yourself or a dependent',
-      buttonContent: 'Get Medicine'},
-    { path: "/Services/SanitationService",
-      image: sanitation,
-      title: 'Sanitation Service',
-      description: 'Request sanitation at a location',
-      buttonContent: 'Request Sanitation'},
-    { path: "/Services/SecurityService",
-      image: security,
-      title: 'Security',
-      description: 'Request security at a location',
-      buttonContent: 'Request Security'},
-    { path: "/Services/GiftDelivery",
+      title: "Flower Delivery",
+      description: "Deliver flowers to a friend or loved one",
+      buttonContent: "Get Flowers",
+    },
+    {
+      path: "/Services/GiftDelivery",
       image: gift,
-      title: 'Gift Delivery',
-      description: 'Send a gift to a friend or loved one',
-      buttonContent: 'Get Gift'},
-    { path: "/Services/DeviceDeliveryService",
-      image: medicalDevice,
-      title: 'Device Delivery',
-      description: 'Get medical devices for yourself or a dependent',
-      buttonContent: 'Get Device'},
-    { path: "/Services/RoomScheduling",
+      title: "Gift Delivery",
+      description: "Send a gift to a friend or loved one",
+      buttonContent: "Get Gift",
+    },
+    {
+      path: "/Services/MedicineDelivery",
+      image: medicine,
+      title: "Medicine Delivery",
+      description: "Get medicine for yourself or a dependent",
+      buttonContent: "Get Medicine",
+    },
+    {
+      path: "/Services/RoomScheduling",
       image: signL,
-      title: 'Room Scheduling',
-      description: 'Schedule a room for yourself or a dependent',
-      buttonContent: 'Schedule Room'},
-    { path: "",
+      title: "Room Scheduling",
+      description: "Schedule a room for yourself or a dependent",
+      buttonContent: "Schedule Room",
+    },
+    {
+      path: "/Services/SanitationService",
+      image: sanitation,
+      title: "Sanitation Service",
+      description: "Request sanitation at a location",
+      buttonContent: "Request Sanitation",
+    },
+    {
+      path: "/Services/SecurityService",
+      image: security,
+      title: "Security",
+      description: "Request security at a location",
+      buttonContent: "Request Security",
+    },
+    {
+      path: "/Services/DeviceDeliveryService",
+      image: medicalDevice,
+      title: "Device Delivery",
+      description: "Get medical devices for yourself or a dependent",
+      buttonContent: "Get Device",
+    },
+    {
+      path: "",
       image: Template,
-      title: 'More Services Soon',
-      description: 'More services to be displayed here, coming soon!',
-      buttonContent: 'Home Page'
-      },
+      title: "More Services Soon",
+      description: "More services to be displayed here, coming soon!",
+      buttonContent: "Home Page",
+    },
   ];
+
+  useEffect(() => {
+    scrollTo(0, 0);
+  }, []);
 
   return (
     <>
-      <TopBanner2 />
       <Box
         sx={{
-          mt: 16,
+          mt: 15,
           backgroundColor: "#ffffff",
         }}
       >
-        <Typography
-          variant={"h1"}
-          align={"center"}
-          color={"#003A96"}
-          //gutterBottom={true}
-          fontSize={36}
-          //fontWeight={'bold'}
-          fontStyle={"inherit"}
-          fontFamily={"sans-serif"}
+        <Box
+          sx={{
+            backgroundColor: "#003A96",
+            position: "relative",
+            marginBottom: "0.5rem",
+            height: "3.5rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            opacity: "90%",
+          }}
         >
-          Services
-        </Typography>
+          <Typography
+            variant={"h1"}
+            color={"white"}
+            //gutterBottom={true}
+            fontSize={46}
+            //fontWeight={'bold'}
+            fontStyle={"inherit"}
+            fontFamily={"sans-serif"}
+          >
+            Services
+          </Typography>
+        </Box>
       </Box>
       <div
         style={{
-          padding: 20,
+          padding: 5,
           display: "flex",
           justifyContent: "center",
           marginLeft: "10%",
           marginRight: "10%",
-
         }}
       >
-        <Box
-        >
-        <Grid container spacing={0} direction={"row"}>
-          {serviceData.map((service:{path: string, image: string, title: string, description: string, buttonContent: string}, index) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={3} key={index}>
-              <Zoom
-                in={showCards}
-                style={{
-                  transitionDelay: showCards ? `${index * 200}ms` : "0ms",
-                }}
-              >
-                <div>
-                  <ServiceCard
-                    servicePath={service.path}
-                    imagePath={service.image}
-                    title={service.title}
-                    description={service.description}
-                    buttonContent={service.buttonContent}
-                  />
-                </div>
-              </Zoom>
-            </Grid>
-          ))}
-        </Grid>
+        <Box>
+          <Grid container spacing={0} direction={"row"}>
+            {serviceData.map(
+              (
+                service: {
+                  path: string;
+                  image: string;
+                  title: string;
+                  description: string;
+                  buttonContent: string;
+                },
+                index,
+              ) => (
+                <Grid item xs={12} sm={8} md={6} lg={4} xl={3} key={index}>
+                  <Zoom
+                    in={showCards}
+                    style={{
+                      transitionDelay: showCards ? `${index * 200}ms` : "0ms",
+                    }}
+                  >
+                    <div>
+                      <ServiceCard
+                        servicePath={service.path}
+                        imagePath={service.image}
+                        title={service.title}
+                        description={service.description}
+                        buttonContent={service.buttonContent}
+                      />
+                    </div>
+                  </Zoom>
+                </Grid>
+              ),
+            )}
+          </Grid>
         </Box>
       </div>
     </>
