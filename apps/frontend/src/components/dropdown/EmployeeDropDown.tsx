905214472eb2d3b0851563b9a7fc2447b9855c7d
jjmurphy1114 <jjmurphy1114@outlook.com>
2024-04-19 16:51:29 -0400

    EmployeeDropDown component works and I added it to flower delivery. Need to fix one small bug with the dropdown staying focused

--- apps/frontend/src/components/dropdown/EmployeeDropDown.tsx
+++ apps/frontend/src/components/dropdown/EmployeeDropDown.tsx
@@ -1,63 +1,64 @@
-import {DropDown} from "./DropDown.tsx";
-import React from "react";
-import {SelectChangeEvent} from "@mui/material";
+import { DropDown } from "./DropDown.tsx";
+import React, { useEffect, useState } from "react";
+import { SelectChangeEvent } from "@mui/material";
 import axios from "axios";
-import {LabelValuePair} from "../../common/LabelValuePair.ts";
+import { LabelValuePair } from "../../common/LabelValuePair.ts";
 
 interface EmployeeDropDownProps {
-  returnedEmployeeID: number;
-  handleChange: (event: SelectChangeEvent) => string;
+    returnedEmployeeID: number;
+    handleChange: (event: SelectChangeEvent) => string;
 }
 
 interface EmployeeData {
-  employeeID: number;
-  firstName: string;
-  lastName: string;
+    employeeID: number;
+    firstName: string;
+    lastName: string;
 }
 
-let EmployeesFromDB: EmployeeData[];
+const EmployeeDropDown: React.FC<EmployeeDropDownProps> = (props) => {
+    const [employeesFromDB, setEmployeesFromDB] = useState<EmployeeData[]>([]);
 
-async function fetchEmployeeData(): Promise<EmployeeData[]> {
-  axios
-    .get<EmployeeData[]>("/api/database/employees")
-    .then((response) => {
-      const employeeIDs = response.data.map((employee) => employee.employeeID);
-      const firstNames = response.data.map((employee) => employee.firstName);
-      const lastNames = response.data.map((employee) => employee.lastName);
+    useEffect(() => {
+        const fetchEmployeeData = async () => {
+            try {
+                const response = await axios.get<EmployeeData[]>("/api/database/employees");
 
+                const employeeIDs = response.data.map((employee) => employee.employeeID);
+                const firstNames = response.data.map((employee) => employee.firstName);
+                const lastNames = response.data.map((employee) => employee.lastName);
 
-      const updatedEmployees: EmployeeData[] = [];
+                const updatedEmployees: EmployeeData[] = [];
 
-      for (let i = 0; i < employeeIDs.length; i++) {
-        updatedEmployees.push({
-          employeeID: employeeIDs[i],
-          firstName: firstNames[i],
-          lastName: lastNames[i],
-        });
-      }
+                for (let i = 0; i < employeeIDs.length; i++) {
+                    updatedEmployees.push({
+                        employeeID: employeeIDs[i],
+                        firstName: firstNames[i],
+                        lastName: lastNames[i],
+                    });
+                }
 
-      return updatedEmployees;
-    })
-    .catch((error) => console.error(error));
-    return [];
-}
+                setEmployeesFromDB(updatedEmployees);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-async function getEmployeeData() {
-    EmployeesFromDB = await fetchEmployeeData();
-}
+        fetchEmployeeData();
+    }, []);
 
-getEmployeeData();
+    const EmployeeDropDownData: LabelValuePair[] = employeesFromDB.map((employee: EmployeeData) => ({
+        value: employee.employeeID,
+        label: `${employee.employeeID}: ${employee.firstName} ${employee.lastName}`
+    }));
 
-//@ts-expect-error Mapping the EmployeeDropDownData array to be an array of LabelValuePair
-const EmployeeDropDownData: LabelValuePair[] = EmployeesFromDB.map((employee: EmployeeData) => ({
-    value: employee.employeeID,
-    label: `${employee.employeeID}: ${employee.firstName} ${employee.lastName}`
-}));
-
-function EmployeeDropDown(props: EmployeeDropDownProps): React.JSX.Element {
-  return(
-    <DropDown items={EmployeeDropDownData} returnData={props.returnedEmployeeID} label={"Employee"} handleChange={props.handleChange} />
-  );
-}
+    return (
+        <DropDown
+            items={EmployeeDropDownData}
+            returnData={props.returnedEmployeeID}
+            label={"Employee"}
+            handleChange={props.handleChange}
+        />
+    );
+};
 
 export default EmployeeDropDown;
