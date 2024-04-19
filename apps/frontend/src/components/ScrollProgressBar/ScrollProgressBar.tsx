2b1303d37552991538f0eb57b8081563dfd6584c
VoDylan <vdylan24@gmail.com>
2024-04-19 17:11:18 -0400

    Added ScrollProgressBar to TopBanner, extra styling still needed

---
+++ apps/frontend/src/components/ScrollProgressBar/ScrollProgressBar.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles({
+  root: {
+    position: 'fixed',
+    width: '100%',
+    paddingTop: '119px',
+    zIndex: -1,
+
+  },
+});
+
+const StatusBar: React.FC = () => {
+  const classes = useStyles();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const computeProgress = () => {
+      // The scrollTop gives length of window that has been scrolled
+      const scrolled = document.documentElement.scrollTop;
+      // scrollHeight gives total length of the window and
+      // The clientHeight gives the length of viewport
+      const scrollLength =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const newProgress = (100 * scrolled) / scrollLength;
+
+      setProgress(newProgress);
+    };
+
+    // Adding event listener on mounting
+    window.addEventListener('scroll', computeProgress);
+
+    // Removing event listener upon unmounting
+    return () => window.removeEventListener('scroll', computeProgress);
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress variant="determinate" value={progress} />
+    </div>
+  );
+};
+
+export default StatusBar;
