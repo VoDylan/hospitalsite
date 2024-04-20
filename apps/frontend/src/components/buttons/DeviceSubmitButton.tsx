35b53d447c75ba6ac17dbb855ba28928746516dd
jjmurphy1114 <jjmurphy1114@outlook.com>
2024-04-20 14:17:40 -0400

    Added dropdown to DeviceDelivery

--- apps/frontend/src/components/buttons/DeviceSubmitButton.tsx
+++ apps/frontend/src/components/buttons/DeviceSubmitButton.tsx
@@ -48,6 +48,8 @@ export function DeviceSubmitButton(props: ButtonProps) {
   async function handleSubmit() {
     if (props.input.roomNum === "") {
       openWithError("Please select a room");
+    } else if (props.input.employeeID === -1){
+      openWithError("Please enter your employee ID");
     } else if (props.input.name === "") {
       openWithError("Please enter your name");
     } else if (props.input.priority === "") {
