8201f86d408759e05fd8124bbc8a8935be819a66
mwalsh001 <matty71604@gmail.com>
2024-04-21 19:47:41 -0400

    Moved back button in slide to inside the paper

--- apps/frontend/src/components/map/TextDirectionsSlide.tsx
+++ apps/frontend/src/components/map/TextDirectionsSlide.tsx
@@ -9,20 +9,21 @@ export default function TextIcon(props: {
   return (
     <>
       <Stack width={"100%"} height={"100%"}>
-        <Button
-          startIcon={<ArrowBackIcon />}
-          onClick={props.handleButtonClick2}
-          variant="text"
-          sx={{
-            width: "25%",
-            justifySelf: "left",
-            marginLeft: "10px",
-          }}
-        >
-          {props.checked2 ? "back" : "back"}
-        </Button>
         <Paper sx={{ width: "100%", height: "100%" }} elevation={4}>
-
+          <Stack>
+            <Button
+              startIcon={<ArrowBackIcon />}
+              onClick={props.handleButtonClick2}
+              variant="text"
+              sx={{
+                width: "25%",
+                justifySelf: "left",
+                marginLeft: "10px",
+              }}
+            >
+              {props.checked2 ? "back" : "back"}
+            </Button>
+          </Stack>
         </Paper>
       </Stack>
     </>
