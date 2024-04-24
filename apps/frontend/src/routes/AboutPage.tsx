5822726501102ead0291a0c8274845fe5f06264c
Rick Wang <rickwjx88@gmail.com>
2024-04-23 22:05:10 -0400

    Working on about page info for people

--- apps/frontend/src/routes/AboutPage.tsx
+++ apps/frontend/src/routes/AboutPage.tsx
@@ -49,8 +49,8 @@ export default function AboutPage() {
         <AboutCardRight
           role={"Team Coach"}
           name={"Connor Olsen"}
-          bio={"Connor is the current team coach for Team F. He is a sophomore double majoring in Math and " +
-            "Computer Science at WPI. Connor helps answer Team F’s questions throughout the development process and " +
+          bio={"Connor is the current team coach for Team F. He is a sophomore double majoring in Computer Science " +
+            "and Math at WPI. Connor helps answer Team F’s questions throughout the development process and " +
             "also conducts regular office hours to address questions of students currently taking CS 3733."}
           email={"caolsen@wpi.edu"}
           imagePath={Connor}
@@ -58,56 +58,73 @@ export default function AboutPage() {
         <AboutCardLeft
           role={"Lead Software Engineer"}
           name={"Elene Kajaia"}
-          bio={""}
+          bio={"Elene is a sophomore double majoring in Computer Science and Robotics at WPI. She has been programming" +
+            "since high school in Java and learned SQL, C, C++, and Python in college. She also has work experience working as " +
+            "a Robotics Process Automation developer. In her free time, she enjoys exploring and cooking."}
           email={"ekajaia@wpi.edu"}
           imagePath={Elene}
         />
         <AboutCardRight
           role={"Assistant Lead"}
           name={"Kyle Schmottlach"}
-          bio={""}
+          bio={"Kyle is a sophomore majoring in Computer Science at WPI. He has been programming since 6th grade and" +
+            "proficient in languages including Java, C++, Python, and SQL. He was also the lead programmer for his high school" +
+            "FIRST robotics team. Outside of school, he enjoys playing the trumpet in a multitude of bands."}
           email={"kjschmottlach@wpi.edu"}
           imagePath={Kyle}
         />
         <AboutCardLeft
           role={"Assistant Lead"}
           name={"Dylan Vo"}
-          bio={""}
+          bio={"Dylan is a sophomore majoring in Computer Science at WPI. He has been programming since high school and" +
+            "is proficient in Java, C, C++, Python, and SQL. He also has internship experience working on quality control " +
+            "for software. Outside of school, he enjoys snowboarding, reading, and playing the violin."}
           email={"dvo@wpi.edu"}
           imagePath={Dylan}
         />
         <AboutCardRight
           role={"Project Manager"}
           name={"Matt Walsh"}
-          bio={""}
+          bio={"Matt is a sophomore majoring in Computer Science at WPI. He has been programming in Java since 8th grade" +
+            "and also is proficient in C, C++, and SQL. He also has experience with managing teams. Outside of school, he " +
+            "enjoys playing sports and weightlifting."}
           email={"mjwalsh1@wpi.edu"}
           imagePath={Matt}
         />
         <AboutCardLeft
           role={"Scrum Master"}
           name={"Robert Mellen"}
-          bio={""}
+          bio={"Robert is a sophomore majoring in Robotics Engineering at WPI. He has been programming since junior year" +
+            "of high school and is proficient in Java, C, and C++. He also has internship experience working as an " +
+            "automation engineer. Outside of school, he enjoys outdoor activities such as running, hiking, and camping."}
           email={"ramellen@wpi.edu"}
           imagePath={Robert}
         />
         <AboutCardRight
           role={"Product Owner"}
           name={"Jingxu (Rick) Wang"}
-          bio={""}
+          bio={"Rick is a junior double majoring in Robotics Engineering and Mechanical Engineering at WPI. He started " +
+            "programming in college and is proficient in Java, C++, and python. He also has internship experience with " +
+            "CAD and DFM. Outside of school, he enjoys playing percussion and traveling."}
           email={"jwang23@wpi.edu"}
           imagePath={Rick}
         />
         <AboutCardLeft
           role={"Documentation Analyst"}
           name={"Arayah Remillard"}
-          bio={""}
+          bio={"Arayah is a junior majoring in Computer Science at WPI. She started programming in college and is " +
+            "proficient with Java, C, and C++. She also has internship experience with documentation and is a tutor" +
+            "for the Writing Center. Outside of school, she enjoys "}
+          boop
           email={"ajremillard@wpi.edu"}
           imagePath={Arayah}
         />
         <AboutCardRight
           role={"Full Time Software Engineer"}
           name={"Sebastian Gurgol"}
-          bio={""}
+          bio={"Sebastian is a junior majoring in Computer Science at WPI. He started programming in high school and " +
+            "is proficient in Java, C, and C++. He has experience working with back end components including experience" +
+            "with SQL. Outside of school, he enjoys transcribing music by ear to make sheet music."}
           email={"sagurgol@wpi.edu"}
           imagePath={Sebastian}
         />
@@ -121,8 +138,9 @@ export default function AboutPage() {
         <AboutCardRight
           role={"Full Time Software Engineer"}
           name={"Jacob Murphy"}
-          bio={"Jacob is a sophomore majoring in Computer Science at WPI. Outside of class, he enjoys power lifting" +
-            "and going to the gym. "}
+          bio={"Jacob is a sophomore majoring in Computer Science at WPI. He has programmed since high school and is " +
+            "proficient in a few different programming languages. He also has had software engineering experience working" +
+            "with a remote team. Outside of class, he enjoys power lifting and going to the gym."}
           email={"jjmurphy1@wpi.edu"}
           imagePath={Jacob}
         />
