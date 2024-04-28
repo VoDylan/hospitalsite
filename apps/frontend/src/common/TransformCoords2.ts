f2fb5f62b9ca837654ba76159b6b952f39b70151
Kyle Schmottlach <kyleschmottlach@gmail.com>
2024-04-28 02:04:37 -0400

    Working coordinates system

--- apps/frontend/src/common/TransformCoords2.ts
+++ apps/frontend/src/common/TransformCoords2.ts
@@ -13,26 +13,9 @@ export default function transformCoords2(
 ) {
   console.log(`mouseX: ${mouseX} | mouseY: ${mouseY} | realContentWidth: ${realContentWidth} | realContentHeight: ${realContentHeight} | windowWidth: ${windowWidth} | windowHeight: ${windowHeight} | rect.width: ${rect.width} | rect.height: ${rect.height}`);
   console.log(transformInfo.current);
-  // const leftOverHeight =
-  //   (windowHeight - 120) / (rect.height / transformInfo.current.scale);
-  //
-  const widthRatio =
-    realContentWidth / (windowWidth - (windowWidth * 0.18));
-  const heightRatio = realContentHeight / (windowHeight - 120);
 
-  // const actualX = Math.round(
-  //   ((mouseX - (windowWidth * 0.18) - transformInfo.current.positionX) *
-  //     transformInfo.current.scale) *
-  //     widthRatio
-  // );
-  // const actualY = Math.round(
-  //   ((mouseY - 120 - transformInfo.current.positionY) *
-  //     transformInfo.current.scale) *
-  //     heightRatio
-  // );
-
-  const actualX = Math.round((mouseX - (windowWidth * 0.18)) * widthRatio);
-  const actualY = Math.round((mouseY - (120)) * heightRatio);
+  const actualX = Math.round(((mouseX - (windowWidth * 0.18)) - transformInfo.current.positionX) / transformInfo.current.scale);
+  const actualY = Math.round(((mouseY - (120)) - transformInfo.current.positionY) / transformInfo.current.scale);
 
   return {actualX, actualY};
 }
